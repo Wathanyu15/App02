5d5a0f7af4bd90fb7fc8799d0337474acbcf6397
Wathanyu Phannon <peepee256111@gmail.com>
2024-07-31 14:05:50 +0700

    first commit

---
+++ src/app/pages/index/index.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton , IonBackButton,IonGrid,IonRow,IonCol,IonIcon} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { cartOutline } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.page.html',
+  styleUrls: ['./index.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton , IonBackButton,IonGrid,IonRow,IonCol,IonIcon ]
+})
+export class IndexPage implements OnInit {
+
+  constructor() {
+    addIcons({ cartOutline });
+  }
+
+  ngOnInit() {
+  }
+
+}
